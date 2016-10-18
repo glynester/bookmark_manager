653d6d76ac65021261fba5bae1300063b5f45f2b
amidabrian51 <briankaty1@me.com>
2016-10-18 10:41:47 +0100

    Completed test for Makers link

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,6 +1,14 @@
+require 'spec_helper'
+
 feature 'Homepage' do
   scenario 'displays links' do
-    visit '/'
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+
+    visit '/links'
+
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
     expect(page).to have_content "Makers Academy"
+    end
   end
 end
