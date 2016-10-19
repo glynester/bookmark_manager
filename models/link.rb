565c3d66b06c2a8c90afc0d514d3c2a3ecf2cdeb
glynester <glynester@hotmail.com>
2016-10-19 13:27:29 +0100

    link file changed to include heroku website.

--- models/link.rb
+++ models/link.rb
@@ -10,8 +10,7 @@ class Link
   property :url,      String
 
 end
-
-DataMapper.setup(:default,"postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 #DataMapper.setup(:default, "postgres://ubuntu:password@localhost/bookmark_manager_test")
 DataMapper.finalize
 DataMapper.auto_upgrade!
