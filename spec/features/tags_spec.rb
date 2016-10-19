b3086e598aea4da44e5e30b033a9648592834cfa
Glenn <glynester@hotmail.com>
2016-10-19 17:32:35 +0000

    1) Added test for tags 2) added code to model app.rb 3) added route to /tags/searched_tag

--- spec/features/tags_spec.rb
+++ spec/features/tags_spec.rb
@@ -1,5 +1,5 @@
 feature "creating links with tags" do
-  scenario " " do
+  scenario "creates a single tag for a link" do
     visit '/links/new'
     fill_in "title", :with => "Ruby Gems"
     fill_in "url", :with => "https://rubygems.org"
@@ -9,4 +9,28 @@ feature "creating links with tags" do
     link = Link.first
     expect(link.tags.map(&:name)).to include('ruby reference')
   end
+end
+
+
+feature "filter links by tag" do
+  
+  before(:each) do
+    Link.create(url: 'http://www.abc.com', title: 'ABC', tags: [Tag.first_or_create(name: 'news')])
+    Link.create(url: 'http://www.google.com', title: 'Google', tags: [Tag.first_or_create(name: 'search')])
+    Link.create(url: 'http://www.sport.com', title: 'Howzatt', tags: [Tag.first_or_create(name: 'cricket')])
+    Link.create(url: 'http://www.onedayint.com', title: 'Hit for 6', tags: [Tag.first_or_create(name: 'cricket')])
+  end
+  
+  scenario "return link which has searched tag" do
+    visit '/tags/bubbles'
+
+    expect(page.status_code).to eq(200)
+    within 'ul#links' do
+      expect(page).not_to have_content('ABC')
+      expect(page).not_to have_content('Google')
+      expect(page).to have_content('Howzatt')
+      expect(page).to have_content('Hit for 6')
+    end
+  end
+    
 end
