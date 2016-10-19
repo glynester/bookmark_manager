286f624e3320db81b1682ae9db112ae7ba70233e
glynester <glynester@hotmail.com>
2016-10-19 23:28:32 +0100

    Changed connection string to Glenn's conn string.

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,6 +4,10 @@ require 'dm-postgres-adapter'
 require_relative '../models/link'
 require_relative '../models/tag'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+#OTHER USER'S CONNECTION STRING
+#DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+
+#GLENN'S CONNECTION STRING
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://postgres:password@localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
