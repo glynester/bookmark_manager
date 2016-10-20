ad1a0029a76bc75ef4ccbd6d40aeeca1cc6a840d
glynester <glynester@hotmail.com>
2016-10-20 23:56:25 +0100

    s23 done. Password authenticated.

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -33,3 +33,22 @@ feature 'User sign up' do
     expect(page).to have_content('Email is already taken')
   end
 end
+
+feature 'User sign in' do
+
+  let!(:user) do
+    User.create(email: 'user@example.com',
+                password: 'secret1234',
+                password_confirmation: 'secret1234')
+  end
+
+  scenario 'with correct credentials' do
+    sign_in(email: user.email,   password: user.password)
+    expect(page).to have_content "Welcome, #{user.email}"
+  end
+
+  it 'does not authenticate when given an incorrect password' do
+    expect(User.authenticate(user.email, 'wrong_stupid_password')).to be_nil
+  end
+
+end
