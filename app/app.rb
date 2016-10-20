f42aefd3296fa112a398a6282456d6d14df68b4c
Stevie Winston-Gore <Floki@Stevies-MBP.home>
2016-10-20 10:47:26 +0100

    debugged stage 17

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,6 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
-
 require_relative 'datamapper_setup'
 
 class BookmarkManager < Sinatra::Base
@@ -18,7 +17,7 @@ class BookmarkManager < Sinatra::Base
   post '/links' do
     link = Link.create(url: params[:url],title: params[:title])
     params[:tags].split.each do |tag|
-      link.tags << Tag.create(name: tag)
+      link.tags << Tag.first_or_create(name: tag)
     end
     link.save
     redirect to('/links')
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,12 +1,9 @@
-
-
 class Link
   include DataMapper::Resource
 
-  has n,  :tags, through: Resource
+  has n, :tags, through: Resource
 
   property :id,      Serial
   property :title,   String
   property :url,     String
-  
 end
