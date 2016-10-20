939f4201efe5f21f4b934f88da84c97d3be44201
amidabrian51 <briankaty1@me.com>
2016-10-20 14:46:45 +0100

    password confirmation

--- spec/user_management_spec.rb
+++ spec/user_management_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 feature 'Sign up' do
   scenario 'create a new user account' do
-    expect { sign_up }.to change(User, :count).by(1)
+    expect { sign_up_right }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, bob@bobross.com!')
     expect(User.first.email).to eq('bob@bobross.com')
+  end
+  scenario 'password mismatch' do
+    expect{ sign_up_wrong }.not_to change(User, :count)
   end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,7 +1,17 @@
-def sign_up
+def sign_up_wrong
   visit '/users/new'
   expect(page.status_code).to eq(200)
   fill_in :email, with: 'bob@bobross.com'
   fill_in :password, with: 'password1'
+  fill_in :password_confirmation, with: 'password2'
+  click_button 'Sign up'
+end
+
+def sign_up_right
+  visit '/users/new'
+  expect(page.status_code).to eq(200)
+  fill_in :email, with: 'bob@bobross.com'
+  fill_in :password, with: 'password1'
+  fill_in :password_confirmation, with: 'password1'
   click_button 'Sign up'
 end
