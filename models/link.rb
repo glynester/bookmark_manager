88487d38f9d6acedd884576e2cceb0a6e25d39ad
glynester <glynester@hotmail.com>
2016-10-20 19:50:31 +0100

    Changed index view to show tags.

--- models/link.rb
+++ models/link.rb
@@ -2,10 +2,10 @@ class Link
 
   include DataMapper::Resource
 
-  has n, :tags, through: Resource
-
   property :id,       Serial
   property :title,    String
   property :url,      String
+
+  has n, :tags, through: Resource
 
 end
